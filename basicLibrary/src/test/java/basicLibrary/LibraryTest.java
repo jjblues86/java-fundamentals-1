c1dd5c814f75091a0a07439b55fca9bfc2890c3f
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-30 11:15:17 -0700

    basicLibrary/src/main/java/basicLibrary/Library.java

--- basicLibrary/src/test/java/basicLibrary/LibraryTest.java
+++ basicLibrary/src/test/java/basicLibrary/LibraryTest.java
@@ -6,7 +6,9 @@ package basicLibrary;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -109,4 +111,40 @@ public class LibraryTest {
         assertEquals("Should return array with lowest average", weeklyMonthTemperatures[0],
                 classUnderTest.arrayOfArrays(weeklyMonthTemperatures));
     }
+
+    // map testing
+    @Test public void uniqueTemperatures() {
+        // Daily average temperatures for Seattle, October 1-28 2017
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String shouldBeResult = "High: 72\n" +
+                "Low: 51\n" +
+                "Never saw temperature: 63\n" +
+                "Never saw temperature: 67\n" +
+                "Never saw temperature: 68\n" +
+                "Never saw temperature: 69";
+
+        assertEquals(" ", shouldBeResult, classUnderTest.analyze2DForValues(weeklyMonthTemperatures));
+    }
+
+    @Test public void winner() {
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+        assertEquals("should equal Bush", "Bush received the most votes!",
+                classUnderTest.tally(votes));
+    }
+
+
 }
